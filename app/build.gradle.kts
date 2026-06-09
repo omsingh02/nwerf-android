fe2764947cd8517f9da722400459e840c68f3ec5
Om Singh <omsgh9450@gmail.com>
2026-06-09 06:37:58 +0530

    Bump version to 0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.omsingh.nwerf"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         vectorDrawables {
             useSupportLibrary = true
